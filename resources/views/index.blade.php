177270622f5c838988ff15a90ed24921a633f086
Pablo Diaz <pabloandi@gmail.com>
2020-12-09 11:11:10 -0500

    lesson finished

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>CSS grid for everyone</title>
+
+        <link rel="stylesheet" href="/css/app.css">
+
+    </head>
+    <body>
+
+        <div class="container">
+            <div class="item">1</div>
+            <div class="item">2</div>
+            <div class="item">3</div>
+            <div class="item">4</div>
+            <div class="item">5</div>
+            <div class="item">6</div>
+            <div class="item">7</div>
+            <div class="item">8</div>
+        </div>
+
+
+    </body>
+</html>
