ee2a9b26bde0954a71937d608c52041f4e3cea79
Pablo Diaz <pabloandi@gmail.com>
2020-12-09 21:18:20 -0500

    lesson finished

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,19 +12,12 @@
     <body>
 
         <div class="container">
-            <div class="box a">
-                <div class="box">a1</div>
-                <div class="box">a2</div>
-                <div class="box">a3</div>
-                <div class="box">a4</div>
-            </div>
-            <div class="box b">B</div>
-            <div class="box c">C</div>
-            <div class="box d">D</div>
-            <div class="box e">E</div>
-            <div class="box f">F</div>
-            <div class="box g">G</div>
-            <div class="box h">H</div>
+            <div class="box">A</div>
+            <div class="box">B</div>
+            <div class="box">C</div>
+            <div class="box">D</div>
+            <div class="box">E</div>
+
         </div>
 
 
